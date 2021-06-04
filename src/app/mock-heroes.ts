5c0d30109dbbdf583ed1897c8b5b74bde6a4bfa3
Oralita <oat431@gmail.com>
2021-06-04 19:13:57 +0700

    [🍇 init] : the angular project

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id:2,name:'Sahachan'},
+    {id:3,name:'Flowero'},
+    {id:4,name:'Oralita'},
+    {id:5,name:'Panomete'},
+    {id:6,name:'Mr. Tippimwong'},
+    {id:7,name:'Oat'},
+    {id:8,name:'Pink Dog'},
+    {id:9,name:'Yellow Flower'},
+]
